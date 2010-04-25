d9e37cc5ba48760423918c8780b269d804e9bda8
aarongustafson <aaron@easy-designs.net>
2010-04-25 11:05:44 -0400

    Rewrite of border-radius extension Addition of box-shadow extension

--- src/eCSStender.css3-backgrounds-and-borders.js
+++ src/eCSStender.css3-backgrounds-and-borders.js
@@ -2,7 +2,7 @@
 Function:       eCSStender.borders-and-backgrounds.js
 Author:         Aaron Gustafson (aaron at easy-designs dot net)
 Creation Date:  2010-04-24
-Version:        0.1
+Version:        0.2
 Homepage:       http://github.com/easy-designs/eCSStender.borders-and-backgrounds.js
 License:        MIT License 
 Note:           If you change or improve on this script, please let us know by
@@ -14,43 +14,52 @@ Note:           If you change or improve on this script, please let us know by
   
   var
   e = eCSStender,
-  
-  UNDEFINED,
-  PROPERTY = 'property',
-  BR       = 'border-radius',
-  MOZ      = '-moz-',
-  WEBKIT   = '-webkit-',
-  KHTML    = '-khtml-',
-  TL       = '-topleft',
-  BTLR     = 'border-top-left-radius',
-  BTRR     = 'border-top-right-radius',
-  BBRR     = 'border-bottom-right-radius',
-  BBLR     = 'border-bottom-left-radius',
-  SPACE    = ' ',
-  COLON    = ': ',
-  SEMICOL  = '; ',
-  THREEPX  = COLON + '3px';
-  
-  e.register(
-    { 'fragment': 'radius',
-      'test':     function()
-      {
-        return ( ! e.isSupported( PROPERTY, BTLR + THREEPX ) &&
-                 ( e.isSupported( PROPERTY, MOZ + BR + TL + THREEPX ) ||
-                   e.isSupported( PROPERTY, WEBKIT + BTLR + THREEPX ) ||
-                   e.isSupported( PROPERTY, KHTML + BTLR + THREEPX ) ) );
-      },
-      'fingerprint': 'net.easy-designs.' + BR
-    },
-    false,
-    function( selector, properties, medium )
-    {
+  // Extensions
+  BRObject = new BorderRadius( e ),
+  BSObject = new BoxShadow( e );
 
+  // Objects
+  function BorderRadius( e )
+  { 
+    var
+    UNDEFINED,
+    PROPERTY = 'property',
+    MOZ      = '-moz-',
+    WEBKIT   = '-webkit-',
+    KHTML    = '-khtml-',
+    SPACE    = ' ',
+    COLON    = ': ',
+    SEMICOL  = '; ',
+    BR       = 'border-radius',
+    TL       = '-topleft',
+    BTLR     = 'border-top-left-radius',
+    BTRR     = 'border-top-right-radius',
+    BBRR     = 'border-bottom-right-radius',
+    BBLR     = 'border-bottom-left-radius',
+    THREEPX  = COLON + '3px';
+    
+    e.register(
+      { fragment: 'radius',
+        test:     function()
+        {
+          return ( ! e.isSupported( PROPERTY, BTLR + THREEPX ) &&
+                   ( e.isSupported( PROPERTY, MOZ + BR + TL + THREEPX ) ||
+                     e.isSupported( PROPERTY, WEBKIT + BTLR + THREEPX ) ||
+                     e.isSupported( PROPERTY, KHTML + BTLR + THREEPX ) ) );
+        },
+        fingerprint: 'net.easy-designs.' + BR
+      },
+      false,
+      apply
+    );  
+    
+    function apply( selector, properties, medium )
+    {
       var
       style_block = selector + ' { ',
       corners;
 
-      // shorthand
+      // handle shorthand
       if ( properties[BR] != UNDEFINED )
       {
         
@@ -80,94 +89,252 @@ Note:           If you change or improve on this script, please let us know by
       style_block += '} ';
 
       e.embedCSS( style_block, medium );
-
     }
-  );
+
+    // =========
+    // UTILITIES
+    // =========
+    function findCorners( value )
+    {
+      var
+      radii = value.split('/'),
+      corners = [];
+      if ( radii.length > 1 )
+      {
+        var horiz = e.trim( radii[0] ).split(SPACE);
+        var vert  = e.trim( radii[1] ).split(SPACE);
+        // handle mirroring
+        horiz = handleMirroring( horiz );
+        vert  = handleMirroring( vert );
+        for ( var i=0; i<4; i++ )
+        {
+          corners[i] = horiz[i] + SPACE + vert[i];
+        }
+      }
+      else
+      {
+        corners = handleMirroring( e.trim( radii[0] ).split(SPACE) );
+      }
+      return corners;
+    }
+    function handleMirroring( radii )
+    {
+      if ( radii.length < 4 )
+      {
+        if ( radii[1] == UNDEFINED ){ radii[1] = radii[0]; }
+        if ( radii[2] == UNDEFINED ){ radii[2] = radii[0]; }
+        if ( radii[3] == UNDEFINED ){ radii[3] = radii[1]; }
+      }
+      return radii;
+    }
+    function assignIndividualCorners( corners )
+    {
+      // webkit/konquerer is a little funky with multiple-assignment
+      if ( e.isSupported( PROPERTY, WEBKIT + BTLR + THREEPX ) ||
+           e.isSupported( PROPERTY, KHTML + BTLR + THREEPX ) )
+      {
+        assignIndividualCorners = function( corners )
+        {
+          return WEBKIT + BTLR + COLON + corners[0] + SEMICOL +
+                 WEBKIT + BTRR + COLON + corners[1] + SEMICOL +
+                 WEBKIT + BBRR + COLON + corners[2] + SEMICOL +
+                 WEBKIT + BBLR + COLON + corners[3] + SEMICOL +
+                 KHTML + BTLR + COLON + corners[0] + SEMICOL +
+                 KHTML + BTRR + COLON + corners[1] + SEMICOL +
+                 KHTML + BBRR + COLON + corners[2] + SEMICOL +
+                 KHTML + BBLR + COLON + corners[3] + SEMICOL;
+        };
+      }
+      else
+      {
+        assignIndividualCorners = function( corners )
+        {
+          return MOZ + BR + TL + COLON + corners[0] + SEMICOL +
+                 MOZ + BR + '-topright: ' + corners[1] + SEMICOL +
+                 MOZ + BR + '-bottomright: ' + corners[2] + SEMICOL +
+                 MOZ + BR + '-bottomleft: ' + corners[3] + SEMICOL;
+        };       
+      }
+      return assignIndividualCorners( corners );
+    }
+    function assignProperty( properties, prop )
+    {
+      if ( e.isSupported( PROPERTY, MOZ + BR + TL + THREEPX ) )
+      {
+        assignProperty = function( properties, prop )
+        {
+          return prop.replace( /border-(top|bottom)-(left|right)-radius/, MOZ + BR + '-$1$2' ) +
+                 COLON + properties[prop] + SEMICOL;
+        };
+      }
+      else
+      {
+        assignProperty = function( properties, prop )
+        {
+          return prop + COLON + properties[prop] + SEMICOL;
+        };
+      }
+      return assignProperty( properties, prop );
+    }
+  }
   
-  function findCorners( value )
+  function BoxShadow( e )
   {
     var
-    radii = value.split('/'),
-    corners = [];
-    if ( radii.length > 1 )
+    UNDEFINED,
+    PROPERTY = 'property',
+    MOZ      = '-moz-',
+    WEBKIT   = '-webkit-',
+    KHTML    = '-khtml-',
+    SPACE    = ' ',
+    COLON    = ': ',
+    SEMICOL  = '; ',
+    BS       = 'box-shadow',
+    FILTER   = 'filter: ',
+    SHADOW   = 'progid:DXImageTransform.Microsoft.Shadow',
+    TWOPX    = '2px',
+    COLOR    = 'rgb(0, 0, 0)',
+    WKTEST   = COLON + COLOR + SPACE + TWOPX + SPACE + TWOPX + SPACE + TWOPX,
+    TEST     = WKTEST + SPACE + TWOPX,
+    IETEST   = FILTER + SHADOW + '(color=#000000,direction=135,strength=3)',
+    
+    // test
+    spread_support = false;
+    
+    e.register(
+      { property: 'box-shadow',
+        test:     function()
+        {
+          return ( ! e.isSupported( PROPERTY, BS + TEST ) &&
+                   ( e.isSupported( PROPERTY, MOZ + BS + TEST ) ||
+                     e.isSupported( PROPERTY, WEBKIT + BS + WKTEST ) ||
+                     e.isSupported( PROPERTY, KHTML + BS + WKTEST ) ||
+                     e.isSupported( PROPERTY, FILTER + IETEST ) ) );
+        },
+        fingerprint: 'net.easy-designs.' + BS
+      },
+      false,
+      apply
+    );  
+    
+    function apply( selector, properties, medium )
     {
-      var horiz = e.trim( radii[0] ).split(SPACE);
-      var vert  = e.trim( radii[1] ).split(SPACE);
-      // handle mirroring
-      horiz = handleMirroring( horiz );
-      vert  = handleMirroring( vert )
-      for ( var i=0; i<4; i++ )
+      var style_block = selector + ' { ';
+
+      style_block += convert( properties['box-shadow'] );
+
+      style_block += '} ';
+
+      e.embedCSS( style_block, medium );
+    }
+    
+    function convert( shadow )
+    {
+      if ( e.isSupported( PROPERTY, FILTER + IETEST ) )
       {
-        corners[i] = horiz[i] + SPACE + vert[i];
+        convert = function( shadow )
+        {
+          var
+          shadows = shadow.split(','),
+          rule    = 'zoom: 1; ' + FILTER,
+          i       = shadows.length,
+          pieces, direction, blur, spread, color;
+          
+          while ( i-- )
+          {
+            shadow = e.trim( shadows[i] ).split(SPACE);
+            pieces = shadow.length;
+            direction = findAngle( shadow[0], shadow[1] );
+            blur = spread = null;
+            
+            // determine what's going on
+            switch ( pieces )
+            {
+              case 5:
+                blur   = e.trim( shadow[2] ).replace(/(\d+).*/,'$1');
+                spread = e.trim( shadow[3] );
+                color  = e.trim( shadow[4] );
+                break;
+              case 4:
+                blur   = e.trim( shadow[2] ).replace(/(\d+).*/,'$1');
+                color  = e.trim( shadow[3] );
+                break;
+              case 3:
+                color  = e.trim( shadow[2] );
+                break;
+            }
+            
+            rule += SHADOW + '(color=' + color + ',direction=' + direction + ',strength=' + blur + ') ';
+          }
+          
+          return rule + SEMICOL;
+          
+        };
       }
-    }
-    else
-    {
-      corners = handleMirroring( e.trim( radii[0] ).split(SPACE) );
-    }
-    return corners;
-  }
-  
-  function handleMirroring( radii )
-  {
-    if ( radii.length < 4 )
-    {
-      if ( radii[1] == UNDEFINED ){ radii[1] = radii[0]; }
-      if ( radii[2] == UNDEFINED ){ radii[2] = radii[0]; }
-      if ( radii[3] == UNDEFINED ){ radii[3] = radii[1]; }
-    }
-    return radii;
-  }
-  
-  function assignIndividualCorners( corners )
-  {
-    // webkit/konquerer is a little funky with multiple-assignment
-    if ( e.isSupported( PROPERTY, WEBKIT + BTLR + THREEPX ) ||
-         e.isSupported( PROPERTY, KHTML + BTLR + THREEPX ) )
-    {
-      assignIndividualCorners = function( corners )
+      else
       {
-        return WEBKIT + BTLR + COLON + corners[0] + SEMICOL +
-               WEBKIT + BTRR + COLON + corners[1] + SEMICOL +
-               WEBKIT + BBRR + COLON + corners[2] + SEMICOL +
-               WEBKIT + BBLR + COLON + corners[3] + SEMICOL +
-               KHTML + BTLR + COLON + corners[0] + SEMICOL +
-               KHTML + BTRR + COLON + corners[1] + SEMICOL +
-               KHTML + BBRR + COLON + corners[2] + SEMICOL +
-               KHTML + BBLR + COLON + corners[3] + SEMICOL;
-      };
+        // test spread support as Webkit doesn't currently have it
+        if ( e.isSupported( PROPERTY, WEBKIT + BS + TEST ) ||
+             e.isSupported( PROPERTY, WEBKIT + BS + TEST ) )
+        {
+          spread_support = true;
+        }
+        
+        convert = function( shadow )
+        {
+          var
+          shadows = shadow.split(','),
+          rules   = [],
+          i       = shadows.length,
+          NULL    = null,
+          pieces, offset, blur, spread, color;
+          
+          while ( i-- )
+          {
+            shadow = e.trim( shadows[i] ).split(SPACE);
+            pieces = shadow.length;
+            offset = shadow[0] + ' ' + shadow[1];
+            blur = spread = NULL;
+            
+            // determine what's going on
+            switch ( pieces )
+            {
+              case 5:
+                blur   = e.trim( shadow[2] );
+                spread = e.trim( shadow[3] );
+                color  = e.trim( shadow[4] );
+                break;
+              case 4:
+                blur   = e.trim( shadow[2] );
+                color  = e.trim( shadow[3] );
+                break;
+              case 3:
+                color  = e.trim( shadow[2] );
+                break;
+            }
+            
+            rules[i] = offset + SPACE +
+                       ( blur != NULL ? blur + SPACE : '' ) +
+                       ( spread_support && spread != NULL ? spread + SPACE : '' ) +
+                       color;
+          }
+          
+          rules = rules.join(', ') + SEMICOL;
+          
+          return WEBKIT + BS + COLON + rules +
+                 KHTML + BS + COLON + rules +
+                 MOZ + BS + COLON + rules;
+          
+        };
+      }
+      return convert( shadow );
     }
-    else
+    
+    function findAngle ( x, y )
     {
-      assignIndividualCorners = function( corners )
-      {
-        return MOZ + BR + TL + COLON + corners[0] + SEMICOL +
-               MOZ + BR + '-topright: ' + corners[1] + SEMICOL +
-               MOZ + BR + '-bottomright: ' + corners[2] + SEMICOL +
-               MOZ + BR + '-bottomleft: ' + corners[3] + SEMICOL;
-      };       
+      return 270 - ( Math.atan2( y, 0-x ) * 180 / Math.PI );
     }
-    return assignIndividualCorners( corners );
+    
   }
-  
-  function assignProperty( properties, prop )
-  {
-    if ( e.isSupported( PROPERTY, MOZ + BR + TL + THREEPX ) )
-    {
-      assignProperty = function( properties, prop )
-      {
-        return prop.replace( /border-(top|bottom)-(left|right)-radius/, MOZ + BR + '-$1$2' ) +
-               COLON + properties[prop] + SEMICOL;
-      };
-    }
-    else
-    {
-      assignProperty = function( properties, prop )
-      {
-        return prop + COLON + properties[prop] + SEMICOL;
-      };
-    }
-    return assignProperty( properties, prop );
-  }
-  
+    
 })();
