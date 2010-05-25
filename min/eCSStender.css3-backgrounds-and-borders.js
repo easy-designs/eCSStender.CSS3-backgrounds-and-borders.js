04eab0a825c9980e7f53fa532b65bd4e55d2fc69
aarongustafson <aaron@easy-designs.net>
2010-05-25 07:15:03 -0400

    forgot to change the version and update the changelog

--- min/eCSStender.css3-backgrounds-and-borders.js
+++ min/eCSStender.css3-backgrounds-and-borders.js
@@ -2,7 +2,7 @@
 Function:       eCSStender.CSS3-backgrounds-and-borders.js
 Author:         Aaron Gustafson (aaron at easy-designs dot net)
 Creation Date:  2010-04-24
-Version:        0.3.1
+Version:        0.3.2
 Homepage:       http://github.com/easy-designs/eCSStender.CSS3-backgrounds-and-borders.js
 License:        MIT License 
 Note:           If you change or improve on this script, please let us know by
